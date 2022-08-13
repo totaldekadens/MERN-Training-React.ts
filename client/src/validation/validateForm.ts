8662ee874ef41f3ff09ad5242fc61928cad2c7f5
Angelica Moberg <angelica.moberg@hotmail.com>
2022-08-13 14:15:49 +0200

    Made it possible to use dash and åäö in lastName and firstName

--- client/src/validation/validateForm.ts
+++ client/src/validation/validateForm.ts
@@ -8,57 +8,67 @@ const validateForm = (player: Player) => {
     ]
 
     // Check required
-    errors.required?.forEach((item: string) => {
+    errors.requiredInputs?.forEach((item: string) => {
+        
         Object.entries(player).forEach(([key, value]) => {
+            
             if(key == item) {
                 if(value == "") {
                     errors[key] = 'Required';
-                }
+                } 
             }
         });
     }) 
 
-    // Check only letters and numbers
+    
     Object.entries(player).forEach(([key, value]) => {
+        
+        // Check only letters
         if(key == "firstName" || key == "lastName") {
-            const checkLetter = onlyLetters(value)
+            const checkLetter = onlyLettersAndDash(value)
             if(!checkLetter) {
                 errors[key] = 'Only letters! Please check';
             }
         } 
 
+        // Check only numbers
         if(key == "phone") {
-            const checkDigits = onlyNumbers(value)
+            if(player.phone) { 
+                const checkDigits = onlyNumbers(value)
 
-            if(!checkDigits) {
-                errors[key] = 'Only numbers! Please check';
+                if(!checkDigits) {
+                    errors[key] = 'Only numbers! Please check';
+                } else if(value.length < 6) {
+                    errors[key] = 'Phonenumber needs to have more than 6 numbers';
+                }
             }
         }
+
+        // Check email
+        if(key == "email") {
+            if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                errors[key] = 'Invalid email address'  // This comes before "Required", check!
+            }
+        }
+
     });
-
-    // Email
-    if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(player.email)) {
-        errors.email = 'Invalid email address'
-    }
-
-    // Phone
-    if(player.phone) {
-        
-        if(player.phone.length < 6) {
-            errors.phone = 'Phonenumber needs to have more than 6 numbers';
-        } 
-    }
 
     return errors;
 } 
 
-
+// Approves only numbers
 export const onlyNumbers = (str: string) => {
     return /^[0-9]+$/.test(str);
 }
 
+// Approves only letters A-Z
 export const onlyLetters = (str: string) => {
     return /^[a-zA-Z]+$/.test(str);
+}
+
+// Approves only letters incl ÅÄÖ and "-"
+export const onlyLettersAndDash = (str: string) => {
+    return /^[A-Za-z\u00C0-\u00ff_-]*$/.test(str);
 }
 
 export default validateForm
